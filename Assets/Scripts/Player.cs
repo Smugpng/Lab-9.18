3d4bd8875b3c20b5372d5bf3dcd6a78d09fc3770
JMUCF <ju398578@ucf.edu>
2024-09-20 00:05:14 -0400

    Switched to new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,49 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public GameObject laserPrefab;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
 
-    private float speed = 6f;
+    private float moveSpeed = 6f;
     private float horizontalScreenLimit = 10f;
     private float verticalScreenLimit = 6f;
     private bool canShoot = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Movement();
-        Shooting();
+        rb.velocity = moveInput * moveSpeed;
+        CheckIfInBounds();
     }
 
-    void Movement()
+    public void Move(InputAction.CallbackContext context)
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
-        {
-            transform.position = new Vector3(transform.position.x * -1f, transform.position.y, 0);
-        }
-        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
-        }
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    void Shooting()
+    public void Shoot(InputAction.CallbackContext context)
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canShoot)
+        if(canShoot)
         {
             Instantiate(laserPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
             canShoot = false;
             StartCoroutine("Cooldown");
+        }
+    }
+
+    void CheckIfInBounds()
+    {
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit) //horizontal bounds checks
+        {
+            transform.position = new Vector3(transform.position.x * -1f, transform.position.y, 0);
+        }
+        
+        else if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit) //vertical bounds checks
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
     }
 
